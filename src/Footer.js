7154afa205c870632b55a3ba24bbd4a25477b906
Kanica Sabharwal <kanicasabharwal94@gmail.com>
2021-03-10 18:28:59 +0000

    Updated Footer and Index

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <h5 className="myName">
+        <a href="https://github.com/Kanica94/weather-react" target="_blank">
+          Open-source code
+        </a>{" "}
+        by{" "}
+        <a href="https://www.linkedin.com/in/kanicasabharwal" target="_blank">
+          Kanica Sabharwal
+        </a>
+      </h5>
+    </div>
+  );
+}
