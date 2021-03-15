9a34925e541ac817f02630ee191005329f1e32ca
Kanica Sabharwal <kanicasabharwal94@gmail.com>
2021-03-15 10:24:04 +0000

    Cleaned up folder structure

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Weather />
-      </header>
+      <Weather />
     </div>
   );
 }
