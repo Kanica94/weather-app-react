cb19af52eba3964c986b9e61a088d09befbe1c3f
Kanica Sabharwal <kanicasabharwal94@gmail.com>
2021-03-17 10:37:59 +0000

    Implemented search function

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
-import FormatDate from "./FormatDate";
-import Footer from "./Footer";
+import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ loaded: false });
+  const [city, setCity] = useState(props.defaultCity);
   function displayTemperature(response) {
     setWeatherData({
       loaded: true,
@@ -22,12 +22,28 @@ export default function Weather(props) {
     });
   }
 
+  function search(city) {
+    const apiKey = "0864ac7dec2540b572d96a9d7503af67";
+    const unit = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+    axios.get(apiUrl).then(displayTemperature);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search(city);
+  }
+
+  function handleCity(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.loaded) {
     return (
       <div className="Weather">
         <div className="container">
           <div className="card-body">
-            <form>
+            <form onSubmit={handleSubmit}>
               <input
                 type="text"
                 placeholder="Enter a City..."
@@ -35,6 +51,7 @@ export default function Weather(props) {
                 autoFocus="on"
                 size="35"
                 className="enter-city"
+                onChange={handleCity}
               />
               <button type="submit" className="btn btn-primary">
                 <i className="fas fa-search" />
@@ -43,123 +60,13 @@ export default function Weather(props) {
                 <i className="fas fa-map-marker-alt" />
               </button>
             </form>
-
-            <h3 className="city-name">
-              {weatherData.city}, {weatherData.country}
-            </h3>
-            <h6 className="date">
-              <FormatDate date={weatherData.date} />
-            </h6>
-
-            <div className="overview">
-              <div className="row">
-                <div className="col-7">
-                  <div className="clearfix main-temperature">
-                    <div className="float-left">
-                      <strong>
-                        <span className="main-temp">
-                          {weatherData.temperature}
-                        </span>
-                      </strong>
-                      <span className="units">
-                        <a href="/" className="celsius-active">
-                          °C
-                        </a>
-                        |
-                        <a href="/" className="fahrenheit-link">
-                          °F
-                        </a>
-                      </span>
-                    </div>
-                    <img
-                      className="float-left"
-                      src={weatherData.imgUrl}
-                      alt={weatherData.description}
-                    />
-                  </div>
-                </div>
-                <div className="col-5">
-                  <ul>
-                    <li className="description">{weatherData.description}</li>
-                    <li>
-                      <strong>
-                        <span className="max">{weatherData.max}° </span>
-                      </strong>
-                      |<span className="min"> {weatherData.min}°</span>
-                    </li>
-                    <li className="wind">Wind: {weatherData.wind} km/h</li>
-                    <li className="humidity">
-                      Humidity: {weatherData.humidity}%
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <br />
-              <hr />
-              <div className="tableone">
-                <div className="row">
-                  <div className="col-2">
-                    Now <br />
-                    <img src={weatherData.imgUrl} width="50px" alt="sun" />
-                    <br />
-                    <span className="degree">10°</span>
-                  </div>
-                  <div className="col-2">
-                    14:00 <br />
-                    <img src={weatherData.imgUrl} width="50px" alt="sun" />
-                    <br />
-                    <span className="degree">8°</span>
-                  </div>
-                  <div className="col-2">
-                    15:00 <br />
-                    <img src={weatherData.imgUrl} width="50px" alt="sun" />
-                    <br />
-                    <span className="degree">8°</span>
-                  </div>
-                  <div className="col-2">
-                    16:00 <br />
-                    <img
-                      src={weatherData.imgUrl}
-                      width="50px"
-                      alt="darkclouds"
-                    />
-                    <br />
-                    <span className="degree">6°</span>
-                  </div>
-                  <div className="col-2">
-                    17:00 <br />
-                    <img
-                      src={weatherData.imgUrl}
-                      width="50px"
-                      alt="darkclouds"
-                    />
-                    <br />
-                    <span className="degree">5°</span>
-                  </div>
-                  <div className="col-2">
-                    18:00 <br />
-                    <img
-                      src={weatherData.imgUrl}
-                      width="50px"
-                      alt="darkclouds"
-                    />
-                    <br />
-                    <span className="degree">4°</span>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <WeatherInfo data={weatherData} />
           </div>
         </div>
-        <Footer />
       </div>
     );
   } else {
-    const apiKey = "0864ac7dec2540b572d96a9d7503af67";
-    const unit = "metric";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${unit}`;
-    axios.get(apiUrl).then(displayTemperature);
-
+    search(city);
     return "Loading weather data...";
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,98 @@
+import React from "react";
+import FormatDate from "./FormatDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h3 className="city-name">
+        {props.data.city}, {props.data.country}
+      </h3>
+      <h6 className="date">
+        <FormatDate date={props.data.date} />
+      </h6>
+
+      <div className="overview">
+        <div className="row">
+          <div className="col-7">
+            <div className="clearfix main-temperature">
+              <div className="float-left">
+                <strong>
+                  <span className="main-temp">{props.data.temperature}</span>
+                </strong>
+                <span className="units">
+                  <a href="/" className="celsius-active">
+                    °C
+                  </a>
+                  |
+                  <a href="/" className="fahrenheit-link">
+                    °F
+                  </a>
+                </span>
+              </div>
+              <img
+                className="float-left"
+                src={props.data.imgUrl}
+                alt={props.data.description}
+              />
+            </div>
+          </div>
+          <div className="col-5">
+            <ul>
+              <li className="description">{props.data.description}</li>
+              <li>
+                <strong>
+                  <span className="max">{props.data.max}° </span>
+                </strong>
+                |<span className="min"> {props.data.min}°</span>
+              </li>
+              <li className="wind">Wind: {props.data.wind} km/h</li>
+              <li className="humidity">Humidity: {props.data.humidity}%</li>
+            </ul>
+          </div>
+        </div>
+        <br />
+        <hr />
+        <div className="tableone">
+          <div className="row">
+            <div className="col-2">
+              Now <br />
+              <img src={props.data.imgUrl} width="50px" alt="sun" />
+              <br />
+              <span className="degree">10°</span>
+            </div>
+            <div className="col-2">
+              14:00 <br />
+              <img src={props.data.imgUrl} width="50px" alt="sun" />
+              <br />
+              <span className="degree">8°</span>
+            </div>
+            <div className="col-2">
+              15:00 <br />
+              <img src={props.data.imgUrl} width="50px" alt="sun" />
+              <br />
+              <span className="degree">8°</span>
+            </div>
+            <div className="col-2">
+              16:00 <br />
+              <img src={props.data.imgUrl} width="50px" alt="darkclouds" />
+              <br />
+              <span className="degree">6°</span>
+            </div>
+            <div className="col-2">
+              17:00 <br />
+              <img src={props.data.imgUrl} width="50px" alt="darkclouds" />
+              <br />
+              <span className="degree">5°</span>
+            </div>
+            <div className="col-2">
+              18:00 <br />
+              <img src={props.data.imgUrl} width="50px" alt="darkclouds" />
+              <br />
+              <span className="degree">4°</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
